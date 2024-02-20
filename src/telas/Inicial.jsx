6922bda63635e842e9996cbe79826981ceea9f81
Murilo Poltronieri Batista De Campos <49900491874@>
2024-02-20 14:16:37 -0300

    jsx

--- src/telas/Inicial.jsx
+++ src/telas/Inicial.jsx
@@ -1,12 +1,24 @@
-import {styles} from './inicial';
-import {View, Text} from 'react-native'
+import { StyleSheet, View, Text} from 'react-native'
 
 export function Inicial(){
     return(
-        <View style={styles.container}>
+        <View style={styles.conteiner}>
             <Text style={styles.texto}>Senai Roberto Mange</Text>
             <Text style={styles.texto}>2DS</Text>
         </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+    conteiner: {
+        flex: 1,
+        backgroundColor: "#080A0C"
+
+    },
+    texto: {
+        color: '#fff',
+        fontSize: 24
+    }
+    
+})
